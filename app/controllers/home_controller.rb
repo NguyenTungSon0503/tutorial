10b037202217886ecc56f5e67b455426b8fda523
“NguyenTungSon0503” <sonmeliodas0503@gmail.com>
2023-03-10 17:24:51 +0700

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=100&convert=USD'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+  end
+end
